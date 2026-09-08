2943e953ad7cdcc72160a6f38fafc05af359dbaf
epilogueeeee <chenxiaofan1206@gmail.com>
2026-09-08 13:48:24 +0800

    控制流练习

--- week01/day2_homework.py
+++ week01/day2_homework.py
@@ -5,12 +5,10 @@ speed_limit = 50
 #count overspeed vehicle number
 def count_overspeed(speeds, speed_limit):
     n_os = 0
-    n_tt = 0
     for s in speeds:
         if s > speed_limit:
             n_os += 1
-        n_tt += 1
-    os_per = n_os / n_tt
+    os_per = n_os / len(speeds) * 100
     return n_os, os_per
 
 n_os_1, os_per_1 = count_overspeed(speeds, speed_limit)
@@ -18,48 +16,39 @@ print(f"{n_os_1} vehicles overspeed, the percentage is {os_per_1:.1f}")
 
 #find max and min of speeds
 def find_max_min(speeds):
-    max = speeds[0]
-    min = speeds[0]
+    max_speed = speeds[0]
+    min_speed = speeds[0]
     for i in speeds:
-        if i > max:
-            max = i
-        if i < min:
-            min = i
-    return max, min
+        if i > max_speed:
+            max_speed = i
+        if i < min_speed:
+            min_speed = i
+    return max_speed, min_speed
 
-max, min = find_max_min(speeds)
-print(f"max speed is {max}, min speed is {min}")
+max_speed, min_speed = find_max_min(speeds)
+print(f"max speed is {max_speed}, min speed is {min_speed}")
 
 #count by interval
-def count_interval(speeds):
-    interval_1 = 0
-    interval_2 = 0
-    interval_3 = 0
-    interval_4 = 0
-    interval_5 = 0
-    for i in speeds:
-        if i < 20:
-            interval_1 += 1
-        elif i < 40:
-            interval_2 += 1
-        elif i < 60:
-            interval_3 += 1
-        elif i < 80:
-            interval_4 += 1
+def count_interval(speeds, bounds):
+    count = [0] * (len(bounds) + 1)
+    for s in speeds:
+        for i, upper in enumerate(bounds):
+            if s < upper:
+                count[i] += 1
+                break
         else:
-            interval_5 += 1
-    return interval_1, interval_2, interval_3, interval_4, interval_5
+            count[-1] += 1
+    return count
 
-val_1, val_2, val_3, val_4, val_5 = count_interval(speeds)
-print(f"[0, 20): {'*' * val_1} {val_1} vehicles\n"
-      f"[20, 40): {'*' * val_2} {val_2} vehicles\n"
-      f"[40, 60): {'*' * val_3} {val_3} vehicles\n"
-      f"[60, 80): {'*' * val_4} {val_4} vehicles\n"
-      f"[80,   ): {'*' * val_5} {val_5} vehicles")
+bounds = [20, 40, 60, 80]
+labels = ["[ 0, 20)", "[20, 40)", "[40, 60)", "[60, 80)", "[80,  ∞)"]
+counts = count_interval(speeds, bounds)
+for lb, c in zip(labels, counts):
+    print(f"{lb}: {'*' * c} {c} vehicles")
 
 #locate overspeed vehicle
 def locate_os(speeds, speed_limit):
-    for i, s in enumerate(speeds, start = 1):
+    for i, s in enumerate(speeds, start=1):
         if s > speed_limit:
             print(f"the overspeed vehicle is number {i}, speed is {s}, over {s - speed_limit} km/h")
     return
